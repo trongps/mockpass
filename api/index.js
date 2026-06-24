21c2854f3cbec557570af591291624323fd198c0
trongps <dev.sytrong@gmail.com>
2026-06-25 01:17:04 +0700

    chore: fix api wrapper

--- api/index.js
+++ api/index.js
@@ -1,16 +1,39 @@
-module.exports = (req, res) => {
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason)
+})
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception thrown:', err)
+})
+
+module.exports = async (req, res) => {
   try {
     const mockpass = require('../app')
     if (!mockpass || !mockpass.app) {
       throw new Error('app is undefined')
     }
+
+    // Catch-all Express error handler just in case
+    // eslint-disable-next-line no-unused-vars
+    mockpass.app.use((err, req, res, next) => {
+      console.error('Express Error:', err)
+      if (!res.headersSent) {
+        res.status(500).json({
+          error: 'Express Error',
+          message: err.message,
+          stack: err.stack,
+        })
+      }
+    })
+
     return mockpass.app(req, res)
   } catch (err) {
+    console.error('Initialization Error:', err)
     res.statusCode = 500
     res.setHeader('Content-Type', 'application/json')
     res.end(
       JSON.stringify({
-        error: 'Initialization or request failed',
+        error: 'Initialization failed',
         message: err ? err.message : 'Unknown error',
         stack: err ? err.stack : '',
       }),
